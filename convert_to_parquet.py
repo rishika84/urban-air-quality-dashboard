39430486e5cbc26b844dcc2b7e513ea97d37fafa
Bhanu Teja Barkam <bhanutejabarkam1848@gmail.com>
2025-07-01 01:02:07 +0100

    Clean repository: Add only essential files for Urban Air Quality Dashboard

--- convert_to_parquet.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Read the CSV file (all columns, let pandas infer dtypes)
-df = pd.read_csv('combined_cleaned_air_quality.csv')
-
-# Save as Parquet for fast loading
-df.to_parquet('combined_cleaned_air_quality.parquet', index=False)
-
-print('Conversion to Parquet complete.') 
--- show_sites.py
+++
@@ -1,5 +0,0 @@
-import duckdb
-
-con = duckdb.connect('air_quality.duckdb')
-sites = con.execute('SELECT DISTINCT "Site Name" FROM air_quality WHERE "Site Name" IS NOT NULL LIMIT 20').fetchdf()
-print(sites) 
